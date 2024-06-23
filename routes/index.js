d6aa82c80555705106ed3660b2d1f2c5bffff06b
FR4N5-SK <fran19062005@gmail.com>
2024-06-22 21:11:09 -0400

    Agregamos las nuevas vistas del sistema

--- routes/index.js
+++ routes/index.js
@@ -11,10 +11,7 @@ router.get('/mostrar/ahorros', function(req, res, next) {
       res.render('ahorros', { data: respuesta.data})
     })
     .catch((error) => {
-      res.status(400).json({
-        status: "400",
-        mensaje: error
-      })
+      res.render('ahorros', { data: []})
     })
 });
 
@@ -25,10 +22,7 @@ router.get('/mostrar/prestamos', function(req, res, next) {
       res.render('prestamos', { data: respuesta.data})
     })
     .catch((error) => {
-      res.status(400).json({
-        status: "400",
-        mensaje: error
-      })
+      res.render('prestamos', { data: []})
     })
 });
 
@@ -44,6 +38,95 @@ router.get('/mostrar/cooperativas', function(req, res, next) {
         mensaje: error
       })
     })
+});
+
+/* Vista para elegir el tipo de entidad del Banco */
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard')
+});
+
+//Vista de agregar cuenta de ahorro
+router.post('/mostrar/ahorros-agregar', function(req, res, next) {
+  ahorrosControllers.crear(req.body)
+  .then((respuesta) => {
+    res.render('pagina-ok', { mensaje: respuesta.mensaje})
+    console.log(respuesta.data)
+    /*res.status(201).json({
+      mensaje: respuesta.mensaje,
+      cuenta_creada: respuesta.data
+    })*/
+  })
+  .catch((error) => {
+    res.render('pagina-error', { mensaje: error})
+  })
+});
+
+// Vista Editar cuenta de ahorros
+router.post('/mostrar/ahorros-editar/:cuenta', function(req, res, next) {
+  ahorrosControllers.editar(req.body, req.params.cuenta)
+  .then((respuesta) => {
+    res.render('pagina-ok', { mensaje: respuesta.mensaje})
+    console.log(respuesta.data)
+  })
+  .catch((error) => {
+    res.render('pagina-error', { mensaje: error})
+  })
+});
+
+
+//Vista Eliminar cuenta de ahorros
+router.get('/mostrar/ahorros-eliminar/:cuenta', function(req, res, next) {
+  ahorrosControllers.eliminar(req.params.cuenta)
+  .then((respuesta) => {
+    res.render('pagina-ok', { mensaje: respuesta.mensaje})
+    console.log(respuesta.data)
+  })
+  .catch((error) => {
+    res.render('pagina-error', { mensaje: error})
+  })
+});
+
+module.exports = router;
+
+//Vista de agregar cuenta de prestamos
+router.post('/mostrar/prestamos-agregar', function(req, res, next) {
+  prestamosControllers.crear(req.body)
+  .then((respuesta) => {
+    res.render('pagina-ok', { mensaje: respuesta.mensaje})
+    console.log(respuesta.data)
+    /*res.status(201).json({
+      mensaje: respuesta.mensaje,
+      cuenta_creada: respuesta.data
+    })*/
+  })
+  .catch((error) => {
+    res.render('pagina-error', { mensaje: error})
+  })
+});
+
+// Vista Editar cuenta de prestamos
+router.post('/mostrar/prestamos-editar/:cuenta', function(req, res, next) {
+  prestamosControllers.editar(req.body, req.params.cuenta)
+  .then((respuesta) => {
+    res.render('pagina-ok', { mensaje: respuesta.mensaje})
+    console.log(respuesta.data)
+  })
+  .catch((error) => {
+    res.render('pagina-error', { mensaje: error})
+  })
+});
+
+
+//Vista Eliminar cuenta de prestamos
+router.get('/mostrar/prestamos-eliminar/:cuenta', function(req, res, next) {
+  prestamosControllers.eliminar(req.params.cuenta)
+  .then((respuesta) => {
+    res.render('pagina-ok', { mensaje: respuesta.mensaje})
+    console.log(respuesta.data)
+  })
+  .catch((error) => {
+    res.render('pagina-error', { mensaje: error})
+  })
 });
 
 module.exports = router;
